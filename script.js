afbb9f5a653057837e12fe41814a0482f7d89124
CacaAlves <cacazinho99@outlook.com>
2020-04-18 13:03:27 -0300

    Initial animation done

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ class Game {
     start() {
         this.menuAnimationOn = false;
 
-        this.camaro = new Car(this,
+        this.mago = new Img(this,
+            document.getElementById("Rei-left"),
+            this.width / 2,
+            this.height - 300 - 10,
+            250,
+            300);
+
+        this.camaro = new Img(this,
             document.getElementById("camaro-img"),
             50, 50, 200, 100);
+        this.initionAnimationBeforeMago = true;
 
         this.CobraSong = document.getElementById("Cobra-Venenosa-audio");
 
@@ -27,15 +35,17 @@ class Game {
             this.mainCharater.position.y = this.height - this.mainCharater.capeHeight - this.mainCharater.height + (this.mainCharater.height / 3);
             this.mainCharater.draw();
             let bus = document.getElementById("bus-img");
-            this.bus = new Car(this,
+            this.bus = new Img(this,
                 bus,
                 (this.mainCharater.position.x + (this.mainCharater.width * 2)),
                 (this.height - (this.mainCharater.height * 3)),
                 (this.mainCharater.width * 4),
                 (this.mainCharater.capeHeight * 2.5)
             );
-            this.bus.draw();
-            this.camaro.draw();
+            if (this.initionAnimationBeforeMago) {
+                this.bus.draw();
+                this.camaro.draw();
+            }
             this.camaro.position.x += 2;
             if (this.camaro.position.x >= this.width) {
                 this.CobraSong.pause();
@@ -43,8 +53,15 @@ class Game {
                 let text1 = "Carai, eu sou um merda, mermão... Eu moro num ônibus!!!";
                 let text2 = "Sou muito pobre... Queria tanto ser o dono da Lage!"
                 let text3 = "Quero mudar de vida... Mas não quero entrar pro crime!";
-                let text4 = "Já sei! Vou salvar meninas aleatórias e perguntar se elas querem morar no meu harém!";
+                let text4 = "Já sei! Vou salvar meninas aleatórias..."
+                let text4_2 =  "e perguntar se elas querem morar no meu harém!";
                 let text5 = "E vou tomar bomba ! HAHAHAHAHHAHA";
+                let text6 = "Olá! Sou o Mago C-Abriu!";
+                let text7 = "Se queres se tornar o dono da Lage";
+                let text8 = "Deves pegar 5 meninas para teu harém";
+                let text9 = "E só assim iniciarás teu reinado!";
+                let text10 = "Deves começar pelo Redondo! :";
+                let text11 = "Com Arthur, o Gado!";
                 let font = "30px Arial";
                 let color = "black";
                 let positionX = this.mainCharater.position.x + 400;
@@ -62,13 +79,49 @@ class Game {
                     this.text3Counter++;
                     this.bus.draw();
                 } else if (this.text4Counter < 400) {
-                    this.drawText(text4, font, color, positionX + 100, positionY);
+                    if (this.text4Counter < 200) {
+                        this.drawText(text4, font, color, positionX + 100, positionY);
+                    }
+                    else {
+                        this.drawText(text4_2, font, color, positionX + 100, positionY);
+                    }
                     this.text4Counter++;
                     this.bus.draw();
-                } else if (this.text5Counter < 100) {
+                } else if (this.text5Counter < 250) {
                     this.drawText(text5, font, color, positionX, positionY);
                     this.text5Counter++;
                     this.bus.draw();
+                } else if (this.text6Counter < 250) {
+                    positionY -= 80;
+                    this.drawText(text6, font, color, positionX, positionY);
+                    this.text6Counter++;
+                    this.mago.draw();
+                    this.initionAnimationBeforeMago = false;
+                } else if (this.text7Counter < 250) {
+                    positionY -= 80;
+                    this.drawText(text7, font, color, positionX, positionY);
+                    this.text7Counter++;
+                    this.mago.draw();
+                } else if (this.text8Counter < 250) {
+                    positionY -= 80;
+                    this.drawText(text8, font, color, positionX, positionY);
+                    this.text8Counter++;
+                    this.mago.draw();
+                } else if (this.text9Counter < 250) {
+                    positionY -= 80;
+                    this.drawText(text9, font, color, positionX, positionY);
+                    this.text9Counter++;
+                    this.mago.draw();
+                } else if (this.text10Counter < 250) {
+                    positionY -= 80;
+                    this.drawText(text10, font, color, positionX, positionY);
+                    this.text10Counter++;
+                    this.mago.draw();
+                } else if (this.text11Counter < 250) {
+                    positionY -= 80;
+                    this.drawText(text11, font, color, positionX, positionY);
+                    this.text11Counter++;
+                    this.mago.draw();
                 } else {
                     this.runGame();
                 }
@@ -80,6 +133,12 @@ class Game {
             this.text3Counter = 0;
             this.text4Counter = 0;
             this.text5Counter = 0;
+            this.text6Counter = 0;
+            this.text7Counter = 0;
+            this.text8Counter = 0;
+            this.text9Counter = 0;            
+            this.text10Counter = 0;            
+            this.text11Counter = 0;
             this.ctx.rect(0, 0, this.width, this.height);
             this.ctx.fillStyle = "rgb(0,0,0)";
             this.ctx.fill();
@@ -118,7 +177,7 @@ class Game {
         this.gameObjects.forEach(object => object.update());
     }
 }
-class Car {
+class Img {
     constructor(game, img, positionX, positionY, width, height) {
         this.game = game;
         this.img = img;
